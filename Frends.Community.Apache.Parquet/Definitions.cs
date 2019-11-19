a0e489fe4b9ec39c3841dd6f4c05c455ae69a1d5
Esa Leppanen <esa.leppanen@hiq.fi>
2019-11-19 16:58:10 +0200

    Initial version - ConvertCsvToParquet implementation

---
+++ Frends.Community.Apache.Parquet/Definitions.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Parquet;
+
+#pragma warning disable CS1591
+
+namespace Frends.Community.Apache.Parquet
+{
+    public class WriteInput
+    {
+        /// <summary>
+        /// Data columns types for Parquet as JSON
+        /// </summary>
+        [DisplayFormat(DataFormatString = "Json")]
+        [DefaultValue(@"[
+    {""name"": ""Id"", ""type"": ""int""},
+    {""name"": ""Name"", ""type"": ""string""},
+    {""name"": ""Desc"", ""type"": ""string""}
+]")]
+        public string Schema { get; set; }
+
+        /// <summary>
+        /// CSV filename
+        /// </summary>
+        [DisplayFormat(DataFormatString = "Text")]
+        [DefaultValue(@"C:\temp\test.csv")]
+        public string CsvFileName { get; set; }
+
+        /// <summary>
+        /// Output filename
+        /// </summary>
+        [DisplayFormat(DataFormatString = "Text")]
+        [DefaultValue(@"c:\temp\test.parquet")]
+        public string OuputFileName { get; set; }
+
+        /// <summary>
+        /// Throw exception if return code of request is not successfull
+        /// </summary>
+        [DefaultValue("true")]
+        public bool ThrowExceptionOnErrorResponse { get; set; } = true;
+    }
+
+    public enum FileEncoding { UTF8, ANSI, ASCII, Unicode, Other }
+
+    public enum CompressionType { Gzip, Snappy, None}
+
+    public class WriteParquetOptions
+    {
+        /// <summary>
+        /// Parquet files row group size. Batch size should be enough large
+        /// because perfomance later
+        /// </summary>
+        [DefaultValue("5000")]
+        public uint ParquetRowGroupSize { get; set; } = 5000;
+
+        /// <summary>
+        /// Parquet Compression type: None / Snappy / GZip (smallest filesize)
+        /// </summary>
+        [DefaultValue(CompressionType.Gzip)]
+        public CompressionType ParquetCompressionMethod { get; set; } = CompressionType.Gzip;
+    }
+
+    public class WriteCSVOptions
+    {
+        /// <summary>
+        /// The separator used in the csv string
+        /// </summary>
+        [DisplayFormat(DataFormatString = "Text")]
+        [DefaultValue("\";\"")]
+
+        public string CsvDelimiter { get; set; }
+        /// <summary>
+        /// This flag tells the reader if there is a header row in the CSV string.
+        /// </summary>
+        [DefaultValue("true")]
+        public bool ContainsHeaderRow { get; set; } = true;
+        
+        /// <summary>
+        /// This flag tells the reader to trim whitespace from the beginning and ending of the field value when reading.
+        /// </summary>
+        [DefaultValue("false")]
+        public bool TrimOutput { get; set; } = false;
+
+        /// <summary>
+        /// This flag indicating if quotes should be ignored when parsing and treated like any other character.
+        /// </summary>
+        [DefaultValue("true")]
+        public bool IgnoreQuotes { get; set; } = true;
+
+        /// <summary>
+        /// The culture info to read/write the entries with, e.g. for decimal separators. InvariantCulture will be used by default. See list of cultures here: https://msdn.microsoft.com/en-us/library/ee825488(v=cs.20).aspx; use the Language Culture Name.
+        /// NOTE: Due to an issue with the CsvHelpers library, all CSV tasks will use the culture info setting of the first CSV task in the process; you cannot use different cultures for reading and parsing CSV files in the same process.|
+        /// </summary>
+        [DisplayFormat(DataFormatString = "Text")]
+        public string CultureInfo { get; set; } = "";
+
+        /// <summary>
+        /// Encoding for the written content. By selecting 'Other' you can use any encoding.
+        /// </summary>
+        public FileEncoding FileEncoding { get; set; }
+
+        [UIHint(nameof(FileEncoding), "", FileEncoding.UTF8)]
+        public bool EnableBom { get; set; }
+
+        /// <summary>
+        /// File encoding to be used. A partial list of possible encodings: https://en.wikipedia.org/wiki/Windows_code_page#List
+        /// </summary>
+        [UIHint(nameof(FileEncoding), "", FileEncoding.Other)]
+        [DisplayFormat(DataFormatString = "Text")]
+        public string EncodingInString { get; set; }
+    }
+
+    public class WriteResult
+    {
+        /// <summary>
+        /// Parquet file was created?
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Status message / error message
+        /// </summary>
+        public string StatusMessage { get; set; }
+
+        /// <summary>
+        /// Output filename
+        /// </summary>
+        public string ParquetFileName { get; set; }
+
+        /// <summary>
+        /// Number of CSV (and Parquet) data rows
+        /// </summary>
+        public int Rows { get; set; }
+    }
+
+
+    public static class Definitions
+    {
+        /// <summary>
+        /// Returns Encoding
+        /// </summary>
+        /// <param name="optionsFileEncoding"></param>
+        /// <param name="optionsEnableBom"></param>
+        /// <param name="optionsEncodingInString"></param>
+        /// <returns></returns>
+        public static Encoding GetEncoding(FileEncoding optionsFileEncoding, bool optionsEnableBom, string optionsEncodingInString)
+        {
+            switch (optionsFileEncoding)
+            {
+                case FileEncoding.Other:
+                    return Encoding.GetEncoding(optionsEncodingInString);
+                case FileEncoding.ASCII:
+                    return Encoding.ASCII;
+                case FileEncoding.ANSI:
+                    return Encoding.Default;
+                case FileEncoding.UTF8:
+                    return optionsEnableBom ? new UTF8Encoding(true) : new UTF8Encoding(false);
+                case FileEncoding.Unicode:
+                    return Encoding.Unicode;
+                default:
+                    throw new ArgumentOutOfRangeException(optionsFileEncoding.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Gets compression method
+        /// </summary>
+        /// <param name="optionsCompressType">Gzip/Snappy/None</param>
+        /// <returns>CompressionMethod</returns>
+        public static CompressionMethod GetCompressionMethod(CompressionType optionsCompressType)
+        {
+            switch (optionsCompressType)
+            {
+                case CompressionType.Gzip:
+                    return CompressionMethod.Gzip;
+                case CompressionType.Snappy:
+                    return CompressionMethod.Snappy;
+                case CompressionType.None:
+                    return CompressionMethod.None;
+                default:
+                    throw new ArgumentOutOfRangeException(optionsCompressType.ToString());
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,106 @@
-# Frends.Community.Apache.Parquet
+# Frends.Community.Azure.Blob
+
+FRENDS Community Task for Apache Parquet related operations.
+
+Task operations that use parquet-dotnet library for writing Parquet files
+https://github.com/elastacloud/parquet-dotnet
+
+- [Installing](#installing)
+- [Tasks](#tasks)
+     - [ConvertCsvToParquet](#ConvertCsvToParquet)
+- [Building](#building)
+- [Contributing](#contributing)
+- [Change Log](#change-log)
+
+# Installing
+
+You can install the task via FRENDS UI Task View or you can find the nuget package from the following nuget feed
+'Insert nuget feed here'
+
+# Tasks
+
+## ConvertCsvToParquet
+Converts CSV file to Parquet format using given metadata.
+
+### Properties
+
+#### Input
+| Property | Type | Description | Example |
+| -------- | -------- | -------- | -------- |
+| Schema | json | Schema for CSV data. Types: boolean, datetime, datetimeoffset, decimal, double, float, int16, int, int32, int64, string and unspecified where ? means nullable column.  | [  {"name": "Id", "type": "int"}, {"name": "Name", "type": "string"}, {"name": "Desc", "type": "string?"} ] |
+| CSV Filename | string |  Full path to the file to be read. | c:\temp\test.csv |
+| Output Filename | string |Full path to the file to be write. | 'UseDevelopmentStorage=true' |
+| Throw exception on error response | bool | Do not handle exceptions if set true. Otherwise catch exception and return error message. | true |
+
+
+#### CSV Options
+| Property | Type | Description | Example |
+| -------- | -------- | -------- | -------- |
+| CSV Delimeter | string | The separator used in the csv string. | ; |
+| ContainsHeaderRow    | bool                 | This flag tells the reader if there is a header row in the CSV string. Default is true. |  true |
+| TrimOutput           | bool                 | This flag tells the reader to trim whitespace from the beginning and ending of the field value when reading.              |  false |
+| Ignore quotes | bool | If value is set, uploaded file will be renamed to this. | true |
+| CultureInfo          | string               | The culture info to parse the file with, e.g. for decimal separators. InvariantCulture will be used by default. See list of cultures [here](https://msdn.microsoft.com/en-us/library/ee825488(v=cs.20).aspx); use the Language Culture Name. <br> NOTE: Due to an issue with the CsvHelpers library, all CSV tasks will use the culture info setting of the first CSV task in the process; you cannot use different cultures for reading and parsing CSV files in the same process.|   |
+| FileEncoding                                | Enum           | Encoding for the read content. By selecting 'Other' you can use any encoding. | |
+| EncodingInString                            | string         | The name of encoding to use. Required if the FileEncoding choice is 'Other'. A partial list of supported encoding names: https://msdn.microsoft.com/en-us/library/system.text.encoding.getencodings(v=vs.110).aspx | `iso-8859-1` |
+
+
+#### Parquet Options
+| Property | Type | Description | Example |
+| -------- | -------- | -------- | -------- |
+| Parquet row group size | number | Parquet files row group size. Batch size should be enough large because perfomance later. | 5000 |
+| Parquet compression method | Enum | Parquet's compression level. GZip (smallest filesize) / Snappy / None | Gzip |
+
+### Returns
+
+| Property | Type | Description | Example |
+| -------- | -------- | -------- | -------- |
+| Success | bool | Operation was successful true/false  | true|
+| StatusMessage | string | "ok" when Success==true. Otherwise returns error message |  "ok" |
+| ParquetFileName | string | Full path to the written file. (Same as Output filename parameter) | c:\temp\test.parquet |
+| Rows | number | Number of handled rows. | 124 |
+
+License
+=======
+This project is licensed under the MIT License - see the LICENSE file for details
+
+Building
+========
+
+Clone a copy of the repo
+
+`git clone https://github.com/CommunityHiQ/Frends.Community.Apache.Parquet`
+
+Restore dependencies
+
+`dotnet restore`
+
+Rebuild the project
+
+`dotnet build` 
+
+Run tests
+
+`dotnet test`
+
+Create a nuget package
+
+`dotnet pack`
+
+Contributing
+============
+When contributing to this repository, please first discuss the change you wish to make via issue, email, or any other method with the owners of this repository before making a change.
+
+1. Fork the repo on GitHub
+2. Clone the project to your own machine
+3. Commit changes to your own branch
+4. Push your work back up to your fork
+5. Submit a Pull request so that we can review your changes
+
+NOTE: Be sure to merge the latest from "upstream" before making a pull request!
+
+# Change Log
+
+| Version | Changes |
+| ----- | ----- |
+| 1.0.4 |Initial version - converts CSV file to Parquet file |
